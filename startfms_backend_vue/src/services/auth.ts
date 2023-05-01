07f8e7eecb06365198eaccca532e1374a870938f
jontcont <52921053+JontCont@users.noreply.github.com>
2023-05-01 17:49:39 +0800

    暫時加入 API

--- startfms_backend_vue/src/services/auth.ts
+++ startfms_backend_vue/src/services/auth.ts
@@ -1,69 +1,100 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import {Gatekeeper} from 'gatekeeper-client-sdk';
 
+import axios from "axios";
 const getError = (error: any) => {
     const message = error.message || 'Failed';
     return new Error(message);
 };
 
 export const loginByAuth = async (email: string, password: string) => {
-    try {
-        const token = await Gatekeeper.loginByAuth(email, password);
+
+    try{
+        const url :string = "https://localhost:5001/api/auth/v1.0/Login/jwtLogin";
+        let token:any;
+        await axios
+            .post(url,{ Account : email, Password : password})
+            .then((res) => {
+                token = res.data;
+                return token;
+            });
+
+        console.log(token);
         return token;
-    } catch (error: any) {
-        throw getError(error);
+    }catch(error : any){
+        throw getError({
+            message : "伺服器暫時無法使用"
+        });
     }
 };
 
 export const registerByAuth = async (email: string, password: string) => {
-    try {
-        const token = await Gatekeeper.registerByAuth(email, password);
-        return token;
-    } catch (error: any) {
-        throw getError(error);
-    }
+    throw getError({
+        message : "伺服器暫時無法使用"
+    });
+
+    // try {
+    //     const token = await Gatekeeper.registerByAuth(email, password);
+    //     return token;
+    // } catch (error: any) {
+    //     throw getError(error);
+    // }
 };
 
 export const loginByGoogle = async () => {
-    try {
-        const token = await Gatekeeper.loginByGoogle();
-        return token;
-    } catch (error: any) {
-        throw getError(error);
-    }
+    throw getError({
+        message : "伺服器暫時無法使用"
+    });
+    // try {
+    //     const token = await Gatekeeper.loginByGoogle();
+    //     return token;
+    // } catch (error: any) {
+    //     throw getError(error);
+    // }
 };
 
 export const registerByGoogle = async () => {
-    try {
-        const token = await Gatekeeper.registerByGoogle();
-        return token;
-    } catch (error: any) {
-        throw getError(error);
-    }
+    getError({
+        message : "伺服器暫時無法使用"
+    });
+    // try {
+    //     const token = await Gatekeeper.registerByGoogle();
+    //     return token;
+    // } catch (error: any) {
+    //     throw getError(error);
+    // }
 };
 
 export const loginByFacebook = async () => {
-    try {
-        const token = await Gatekeeper.loginByFacebook();
-        return token;
-    } catch (error: any) {
-        throw getError(error);
-    }
+    throw getError({
+        message : "伺服器暫時無法使用"
+    });
+    // try {
+    //     const token = await Gatekeeper.loginByFacebook();
+    //     return token;
+    // } catch (error: any) {
+    //     throw getError(error);
+    // }
 };
 
 export const registerByFacebook = async () => {
-    try {
-        const token = await Gatekeeper.registerByFacebook();
-        return token;
-    } catch (error: any) {
-        throw getError(error);
-    }
+    throw getError({
+        message : "伺服器暫時無法使用"
+    });
+    // try {
+    //     const token = await Gatekeeper.registerByFacebook();
+    //     return token;
+    // } catch (error: any) {
+    //     throw getError(error);
+    // }
 };
 export const getProfile = async () => {
-    try {
-        const user = await Gatekeeper.getProfile();
-        return user;
-    } catch (error: any) {
-        throw getError(error);
-    }
+    throw getError({
+        message : "伺服器暫時無法使用"
+    });
+    // try {
+    //     const user = await Gatekeeper.getProfile();
+    //     return user;
+    // } catch (error: any) {
+    //     throw getError(error);
+    // }
 };
