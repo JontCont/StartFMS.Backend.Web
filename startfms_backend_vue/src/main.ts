b1bf5afba955264e7999bee7cd532f7bb00ed166
jontcont <52921053+JontCont@users.noreply.github.com>
2023-05-01 17:49:20 +0800

    移除 Gatekeeper

--- startfms_backend_vue/src/main.ts
+++ startfms_backend_vue/src/main.ts
@@ -5,14 +5,11 @@ import store from './store';
 import {i18n} from './translation';
 
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-import {Gatekeeper} from 'gatekeeper-client-sdk';
 import Toast, {PluginOptions} from 'vue-toastification';
 import {VueWindowSizePlugin} from 'vue-window-size/option-api';
 import {ProfabricComponents} from '@profabric/vue-components';
 
 import './index.scss';
-
-Gatekeeper.initialize('de378d9c-38c8-42c1-b961-9e4fa92d6a5e');
 
 const options: PluginOptions = {
     timeout: 5000,
