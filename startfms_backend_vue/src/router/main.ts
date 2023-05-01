f7269dfeb493b6c3a186f997531c626ac02ad894
jontcont <52921053+JontCont@users.noreply.github.com>
2023-05-01 12:05:22 +0800

    clean data

---
+++ startfms_backend_vue/src/router/main.ts
@@ -0,0 +1,13 @@
+import HelloWorldVue from "@/components/HelloWorld.vue";
+
+
+const routes =[
+  {
+    path : '/',
+    name : 'home',
+    component : HelloWorldVue,
+    meta:{
+
+    }
+  },
+];
